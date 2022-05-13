6ba23a742167eb5e21c567fc4fa03db906bbf827
Zarino Zappia <mail@zarino.co.uk>
2022-05-13 08:38:05 +0100

    Animated progress bars

--- static/js/all.js
+++ static/js/all.js
@@ -11,4 +11,13 @@ $(function(){
             size: 1.5
         })
     })
+
+    $('.js-progress-animated').each(function(){
+        var $p = $(this)
+        var width = ($p.attr('aria-valuenow') / $p.attr('aria-valuemax') * 100)
+        $p.css('width', '0%')
+        setTimeout(function(){
+            $p.css('width', '' + width + '%')
+        }, 100)
+    })
 })
